49bf138c86b253bd879c42b592de2388334d1aeb
Jacob Greenfield <jacob@jacobgreenfield.me>
2019-01-19 18:09:22 -0500

    Clean up `Bounded` trait

--- src/ops/bounded.rs
+++ src/ops/bounded.rs
@@ -1,5 +1,3 @@
-use std::num::Wrapping;
-
 /// Numbers which have upper and lower bounds
 pub trait Bounded {
     // FIXME (#5527): These should be associated constants
@@ -7,13 +5,4 @@ pub trait Bounded {
     fn min_value() -> Self;
     /// returns the largest finite number this type can represent
     fn max_value() -> Self;
-}
-
-impl<T: Bounded> Bounded for Wrapping<T> {
-    fn min_value() -> Self {
-        Wrapping(T::min_value())
-    }
-    fn max_value() -> Self {
-        Wrapping(T::max_value())
-    }
 }
